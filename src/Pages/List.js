75cf3ad6f3913c79cc0503936380d3f159a7bc07
mushmulcho <alex.goychev@gmail.com>
2019-09-21 18:55:35 +0300

    page visualization

--- src/Pages/List.js
+++ src/Pages/List.js
@@ -8,10 +8,13 @@ class List extends Component{
 
     render(){
         let moviesList = this.props.data.map( (movie) => {
+            console.log('movie', movie)
             return (
                 <div className="movieBox" key={movie.Title} id={movie.Title} onClick={this.props.onMovieClick}>
-                  <div className="movieBox-header">{movie.Title}</div>
-                    <img className="image small" src={movie.Poster}/>
+                    <div className="small title">{movie.Title}</div>
+                    <div className="small year">Year : {movie.Year}</div>
+                    <div className="small type">Type : {movie.Type}</div>
+                    <div><img className="image-small" src={movie.Poster}/></div>
                 </div>
             )
         })
--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -16,20 +16,62 @@ class Page extends Component {
         }
         return allInfo;
     }
+    test(info) {
+        if(!info.Response)
+            return (<div></div>);
+
+        return(
+            <div className="movie-page-box" key={info.imdbID}>
+                <div className="title-overview">
+                    <h1>{info.Title}</h1>
+                    <div>
+                        {info.Runtime} | {info.Type}
+                    </div>
+                </div>
+                <div className="body-poster">
+                    <div className="movie-poster">
+                        <img className="image-big" src={info.Poster}/>
+                    </div>
+                    <div className="body-overview">
+                        <h5>
+                            {info.Plot}
+                        </h5>
+                        <div className="podcast">
+                            <h6>Director : {info.Director}</h6>
+                            <h6>Writer : {info.Writer}</h6>
+                            <h6>Actors : {info.Actors}</h6>
+                            <h6>Genre : {info.Genre}</h6>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className="footer-overview">
+                    <h6>Language : {info.Language}</h6>
+                    <h6>Country : {info.Country}</h6>
+                    <h6>Awards : {info.Awards}</h6>
+                    <h6>Metascore : {info.Metascore}</h6>
+                    <h6>imdbRating : {info.imdbRating}</h6>
+                    <h6>imdbVotes : {info.imdbVotes}</h6>
+
+                </div>
+            </div>
+        )
+    }
     render(){
-        let allInfo = [];
-        let info = "";
-        for(let key in this.props.movieInfo){
-            info = this.props.movieInfo[key];
-            console.log(info)
-            if(typeof info == "number" || typeof info =="string"){
-                allInfo.push(
-                    <div key={key}>{key} - {info}</div>
-                ) 
-            }
+        // let allInfo = [];
+        // let info = "";
+        // for(let key in this.props.movieInfo){
+        //     info = this.props.movieInfo[key];
+        //     console.log(info)
+        //     if(typeof info == "number" || typeof info =="string"){
+        //         allInfo.push(
+        //             <div key={key}>{key} - {info}</div>
+        //         ) 
+        //     }
             
-        }
-        return allInfo;
+        // }
+        // return allInfo;
+        return this.test(this.props.movieInfo)
     }
 }
 export default Page
